6d3eb0d76768b8032f5448f86ca7997e24bcd258
youngsoo shin <sys920@gmail.com>
2019-01-22 00:30:21 -0600

    update css and fetch to get 10 recipes that match your query

--- app.js
+++ app.js
@@ -1,46 +1,34 @@
 
   const htmlElement = document.getElementById('container');
-  let myrecipes = [];
 
-  fetch(` http://api.yummly.com/v1/api/recipes?_app_id=db56cf30&_app_key=b32e160b791f2d418eac389c01d9e2dd&allowedIngredient[]=beef&allowedCuisine[]=cuisine^cuisine-france`)
+
+  fetch(`http://api.yummly.com/v1/api/recipes?_app_id=db56cf30&_app_key=b32e160b791f2d418eac389c01d9e2dd&allowedIngredient[]=beef&allowedCuisine[]=cuisine^cuisine-france`)
   .then(response => response.json())
   .then((json) => {
-
-    json.matches.forEach((element) => {  
-      myrecipes.push(element.id);
+    let recipes = json.matches.map((recipe) => {
+      return fetch(`http://api.yummly.com/v1/api/recipe/${recipe.id}?_app_id=db56cf30&_app_key=b32e160b791f2d418eac389c01d9e2dd`)
+        .then(response => response.json())
     });
 
-    Promise.all(myrecipes).then((myResult) => {   
 
-      myResult.forEach((ele) => {
 
-        fetch(`http://api.yummly.com/v1/api/recipe/${ele}?_app_id=db56cf30&_app_key=b32e160b791f2d418eac389c01d9e2dd`)
-        .then(response => response.json())   
-        .then((json) => {
+    Promise.all(recipes).then((recipes) => {   
+      recipes.forEach((recipe) => {
 
-          let str1 ='' ;
-          let str2 ='' ;
-          let imgUrl = json.images.map(img => img.hostedLargeUrl)
+        htmlElement.insertAdjacentHTML('afterbegin',
 
-          for ( let i = 0 ; i < json.ingredientLines.length ; i++ ) {
-            str1 = str1 + `<li> ${json.ingredientLines[i]}</li>`
-           }           
-        
-           str2 = `
-            <div id="content">
-              <li><h3>${json.name}</h3></li>
-              <img  src="${imgUrl}" width ="100%">
-              <li class ="ingredient"> ${str1}</li>     
-              <li><p></p></li>
-              <li class ="url">Source:<a href ="${json.source.sourceRecipeUrl}" target="new">${json.source.sourceSiteUrl}</li>
-            </div>`
-
-            htmlElement.insertAdjacentHTML('beforeend',str2)
-
-        });
-
+          `<div id="content">
+            <li>
+              <h3>${recipe.name}</h3>
+              <img src='${recipe.images[0].hostedLargeUrl}'>
+              <h3 class ="ingredient" >Ingredients</h5>
+              <ul>
+                ${recipe.ingredientLines.map(ingredient => `<li>${ingredient}</li>`).join('')}
+              </ul>
+               <span class ="url">Get the recipe at <a href='${recipe.source.sourceRecipeUrl}'>${recipe.source.sourceDisplayName}</a> </span>
+            </li>
+          </div>`
+        )
       });
-
-    });   
-
+    })
   });
